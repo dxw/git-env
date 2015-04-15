b398a51ab6fbe80a142bc0693cc402d7be08bddb
Tom Adams <tom@dxw.com>
2015-04-15 10:27:29 -0400

    Remove [COMMAND] from help's own documentation

--- git-env.go
+++ git-env.go
@@ -206,7 +206,7 @@ func help(arg string) {
 	// case "deploy":
 	default:
 		fmt.Println("Commands:")
-		fmt.Println("  git env help [COMMAND]                     - show help")
+		fmt.Println("  git env help                               - show this help")
 		fmt.Println("  git env init                               - configure which ENV branches are being used")
 		fmt.Println("  git env start BRANCH_NAME                  - start a new feature branch")
 		fmt.Println("  git env deploy ENV_BRANCH [FEATURE_BRANCH] - deploy a feature branch to an ENV branch (FEATURE_BRANCH defaults to current branch)")
